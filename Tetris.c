ba33d45e1c2345acfb82c540312c29a5b956c460
SeminLinux <ohsemin4@gmail.com>
2018-11-25 19:36:39 +0900

    control Tetris

--- Tetris.c
+++ Tetris.c
@@ -2,11 +2,12 @@
 #include <locale.h>
 #include <ncursesw/curses.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define LEFTEDGE 20
 #define RIGHTEDGE 18
 #define ROW 20
-
+#define SPACE 32
 
 void interface();
 void singlemode();
@@ -15,58 +16,71 @@ void bye();
 void a();
 void b();
 void c();
-int mode = 0;
+int kbhit(void);
+void draw_Borad(int, int);
+void new_block();
+void key_left();
+void key_right();
+void key_down();
+void key_up();
+int crush_check(int, int, int);
+void block_inactive();
+void delete_block();
 
-int Board[20][13] = {
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,0,0,0,0,0,0,0,0,0,0,0,1},
-{1,1,1,1,1,1,1,1,1,1,1,1,1},
+int mode = 0;
+int block_xpos, block_ypos, block_rotate, crush_flag = 0, before_inactive_check = 0; //ingame blcok x_pos, y_pos, block_rorate, crush_flag
+int new_block_flag = 1;
+int Board[20][16] = {
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
+int Real_game_Board[20][16];
 int Block[7][4][5][5] = {
 	{
 		{
 			{0,0,0,0,0},
 			{0,0,0,0,0},
-			{0,1,0,0,0},
-			{0,1,1,1,0},
+			{0,2,0,0,0},
+			{0,2,2,2,0},
 			{0,0,0,0,0}
 		},//"ㄴ블록"
 		{
 			{0,0,0,0,0},
-			{0,0,0,1,0},
-			{0,0,0,1,0},
-			{0,0,1,1,0},
+			{0,0,0,2,0},
+			{0,0,0,2,0},
+			{0,0,2,2,0},
 			{0,0,0,0,0}
 		},//"회전"
 		{
 			{0,0,0,0,0},
-			{0,1,1,1,0},
-			{0,0,0,1,0},
+			{0,2,2,2,0},
+			{0,0,0,2,0},
 			{0,0,0,0,0},
 			{0,0,0,0,0}
 		},//"회전"
 		{
 			{0,0,0,0,0},
-			{0,1,1,0,0},
-			{0,1,0,0,0},
-			{0,1,0,0,0},
+			{0,2,2,0,0},
+			{0,2,0,0,0},
+			{0,2,0,0,0},
 			{0,0,0,0,0}
 		}//"회전"
 	},//"ㄴ 블록"
@@ -74,29 +88,29 @@ int Block[7][4][5][5] = {
 		{
 			{0,0,0,0,0},
 			{0,0,0,0,0},
-			{0,0,0,1,0},
-			{0,1,1,1,0},
+			{0,0,0,2,0},
+			{0,2,2,2,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,0,1,1,0},
-			{0,0,0,1,0},
-			{0,0,0,1,0},
+			{0,0,2,2,0},
+			{0,0,0,2,0},
+			{0,0,0,2,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,1,1,1,0},
-			{0,1,0,0,0},
+			{0,2,2,2,0},
+			{0,2,0,0,0},
 			{0,0,0,0,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,1,0,0,0},
-			{0,1,0,0,0},
-			{0,1,1,0,0},
+			{0,2,0,0,0},
+			{0,2,0,0,0},
+			{0,2,2,0,0},
 			{0,0,0,0,0}
 		}
 	},//"j 블록"
@@ -104,29 +118,29 @@ int Block[7][4][5][5] = {
 		{
 			{0,0,0,0,0},
 			{0,0,0,0,0},
-			{0,0,1,1,0},
-			{0,0,1,1,0},
+			{0,0,2,2,0},
+			{0,0,2,2,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
 			{0,0,0,0,0},
-			{0,0,1,1,0},
-			{0,0,1,1,0},
+			{0,0,2,2,0},
+			{0,0,2,2,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
 			{0,0,0,0,0},
-			{0,0,1,1,0},
-			{0,0,1,1,0},
+			{0,0,2,2,0},
+			{0,0,2,2,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
 			{0,0,0,0,0},
-			{0,0,1,1,0},
-			{0,0,1,1,0},
+			{0,0,2,2,0},
+			{0,0,2,2,0},
 			{0,0,0,0,0}
 		}
 	},//"ㅁ블록"
@@ -134,89 +148,89 @@ int Block[7][4][5][5] = {
 		{
 			{0,0,0,0,0},
 			{0,0,0,0,0},
-			{0,0,1,0,0},
-			{0,1,1,1,0},
+			{0,0,2,0,0},
+			{0,2,2,2,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,0,0,1,0},
-			{0,0,1,1,0},
-			{0,0,0,1,0},
+			{0,0,0,2,0},
+			{0,0,2,2,0},
+			{0,0,0,2,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,1,1,1,0},
-			{0,0,1,0,0},
+			{0,2,2,2,0},
+			{0,0,2,0,0},
 			{0,0,0,0,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,1,0,0,0},
-			{0,1,1,0,0},
-			{0,1,0,0,0},
+			{0,2,0,0,0},
+			{0,2,2,0,0},
+			{0,2,0,0,0},
 			{0,0,0,0,0}
 		}
 	},//"t블록"
 	{
 		{
 			{0,0,0,0,0},
-			{0,1,1,0,0},
-			{0,0,1,1,0},
+			{0,2,2,0,0},
+			{0,0,2,2,0},
 			{0,0,0,0,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,0,0,1,0},
-			{0,0,1,1,0},
-			{0,0,1,0,0},
+			{0,0,0,2,0},
+			{0,0,2,2,0},
+			{0,0,2,0,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,1,1,0,0},
-			{0,0,1,1,0},
+			{0,2,2,0,0},
+			{0,0,2,2,0},
 			{0,0,0,0,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,0,0,1,0},
-			{0,0,1,1,0},
-			{0,0,1,0,0},
+			{0,0,0,2,0},
+			{0,0,2,2,0},
+			{0,0,2,0,0},
 			{0,0,0,0,0}
 		}
 	},//"ㄹ블록"
 	{
 		{
 			{0,0,0,0,0},
-			{0,0,1,1,0},
-			{0,1,1,0,0},
+			{0,0,2,2,0},
+			{0,2,2,0,0},
 			{0,0,0,0,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,0,1,0,0},
-			{0,0,1,1,0},
-			{0,0,0,1,0},
+			{0,0,2,0,0},
+			{0,0,2,2,0},
+			{0,0,0,2,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,0,1,1,0},
-			{0,1,1,0,0},
+			{0,0,2,2,0},
+			{0,2,2,0,0},
 			{0,0,0,0,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,0,1,0,0},
-			{0,0,1,1,0},
-			{0,0,0,1,0},
+			{0,0,2,0,0},
+			{0,0,2,2,0},
+			{0,0,0,2,0},
 			{0,0,0,0,0}
 		}
 	},//"reverse ㄹ블록"
@@ -224,34 +238,33 @@ int Block[7][4][5][5] = {
 		{
 			{0,0,0,0,0},
 			{0,0,0,0,0},
-			{1,1,1,1,0},
+			{2,2,2,2,0},
 			{0,0,0,0,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,0,1,0,0},
-			{0,0,1,0,0},
-			{0,0,1,0,0},
-			{0,0,1,0,0}
+			{0,0,2,0,0},
+			{0,0,2,0,0},
+			{0,0,2,0,0},
+			{0,0,2,0,0}
 		},
 		{
 			{0,0,0,0,0},
 			{0,0,0,0,0},
-			{1,1,1,1,0},
+			{2,2,2,2,0},
 			{0,0,0,0,0},
 			{0,0,0,0,0}
 		},
 		{
 			{0,0,0,0,0},
-			{0,0,1,0,0},
-			{0,0,1,0,0},
-			{0,0,1,0,0},
-			{0,0,1,0,0}
+			{0,0,2,0,0},
+			{0,0,2,0,0},
+			{0,0,2,0,0},
+			{0,0,2,0,0}
 		}
 	}//"ㅡ 블록"
 };
-
 
 int main(){
 
@@ -260,9 +273,7 @@ int main(){
 	interface();
 	return 0;
 }
-
-void interface()
-{
+void interface(){
 	int ch,count=1;
 	
 	initscr();
@@ -276,7 +287,7 @@ void interface()
 	mvprintw(3 ,10,"@@@@@@@@@@              @@@@@@@@@@              @@@@@@@@@@              @@@@@@@@@@              @@@@@@@@@@              @@@@@@@@@@");
 	mvprintw(4 ,10,"@@@@@@@@@@              @@@@@@@@@@              @@@@@@@@@@              @@@@@@@@@@              @@@@@@@@@@              @@@@@@@@@@");
 	mvprintw(5 ,10,"    @@                  @@                          @@                  @@      @@                  @@                  @@        ");
-        mvprintw(6 ,10,"    @@                  @@                          @@                  @@      @@                  @@                  @@        ");
+	mvprintw(6 ,10,"    @@                  @@                          @@                  @@      @@                  @@                  @@        ");
 	mvprintw(7 ,10,"    @@                  @@@@@@@@@@                  @@                  @@@@@@@@@@                  @@                  @@@@@@@@@@");
 	mvprintw(8 ,10,"    @@                  @@@@@@@@@@                  @@                  @@@@@@@@@@                  @@                  @@@@@@@@@@");
 	mvprintw(9 ,10,"    @@                  @@                          @@                  @@  @@@                     @@                           @");
@@ -350,8 +361,7 @@ void interface()
 	//endwin();	
 
 }
-void singlemode()
-{
+void singlemode(){
 	standend();
 	mvprintw(24, 60,"*******************************");
 	standout();
@@ -362,8 +372,7 @@ void singlemode()
 	mvprintw(28, 60,"*******************************");
 	refresh();
 }
-void vsmode()
-{
+void vsmode(){
 	standend();
 	mvprintw(24, 60,"*******************************");
 	mvprintw(25, 60,"            싱글 MODE          ");
@@ -375,8 +384,7 @@ void vsmode()
 	refresh();
 }
 
-void bye()
-{
+void bye(){
 	standend();
 	mvprintw(24, 60,"*******************************");
 	mvprintw(25, 60,"            싱글 MODE          ");
@@ -388,64 +396,276 @@ void bye()
 	refresh();
 }
 
-void a()
-{
-	
-	
-	char wall[] = "@";	char blank[]   = "   ";
+void a(){
 	char tetris[]  = "TETIRS";
-	char fill[] = "*";
-	int i, j;
-	int pos = 0;
+	int i, j, ch;
+	int y_pos = 0;
+	int x_pos = 0;
 	int dir = 1;
 	clear();
-	move(pos,8);
+	cbreak();
+	noecho();
+	nodelay(stdscr,TRUE);
+
+	scrollok(stdscr, TRUE);
+
+	move(y_pos,x_pos + 8);//move(y, x); 
 	addstr(tetris);
-	pos += dir;
-	pos += dir;
 	refresh();
-	sleep(2);
-	for(i = 0; i<40;i++){
-		for(j = 0; j<26; j++){
-			move(pos + i, LEFTEDGE+j);
-			if(Board[i/2][j/2] == 0)
-				addstr(blank);
-			else if(Board[i/2][j/2] == 1){
-				//standout();
-				addstr(wall);
+	y_pos += 2;
+	for(int i = 0; i<20; i++)
+		for(int j = 0; j<16; j++)
+			Real_game_Board[i][j] = Board[i][j];
+	for(int i = 0 ; i< 20; i++)
+		for(int j = 0; j<16;j++)
+			Board[i][j] = 100;
+	//EMPTY BOARD draw
+	//draw_Borad(y_pos, x_pos);	
+	//refresh();
+	/*clear();
+	refresh();
+	endwin();
+	for(int i = 0; i<20; i++){
+		for(int j = 0; j<13; j++){
+			printf("%d " ,Real_game_Board[i][j]);
+		}
+		printf("\n");
+	}
+	break;*/
+	while(1){
+		if(new_block_flag == 1){
+			new_block();
+			
+		}		
+		draw_Borad(y_pos, x_pos);
+		if(kbhit()){
+			ch = getch();
+			switch(ch){//getch() 
+				case KEY_DOWN :
+					if(crush_check(block_xpos, block_ypos+1,block_rotate) == true){
+						before_inactive_check = 0;
+						key_down();
+					}									
+					break;
+				case KEY_UP :
+					if(crush_check(block_xpos, block_ypos,(block_rotate+1)%4) == true){
+						key_up();	
+					}				
+					break;
+				case KEY_RIGHT :
+					if(crush_check(block_xpos+1, block_ypos,block_rotate) == true){
+						key_right();
+					}
+					break;
+				case KEY_LEFT :
+					if(crush_check(block_xpos-1, block_ypos,block_rotate) == true){
+						key_left();
+					}
+					//draw_Borad(y_pos, x_pos);
+					break;
+				case SPACE :
+					while(crush_check(block_xpos, block_ypos+1,block_rotate) == true){
+						key_down();
+					}
+					before_inactive_check = 3;
+					break;
+				default:
+					break;
 			}
-			refresh();
-			//sleep(1);
-			
+			if(crush_check(block_xpos,block_ypos+1, block_rotate)==false&&(before_inactive_check == 3)){//check_inactive
+			block_inactive();
+			delete_block();				
+			}
+			else if(crush_check(block_xpos,block_ypos+1, block_rotate)==false){
+				before_inactive_check++;
+			}
 		}
-	}
-	refresh();
-	for( i = 0; i< 5; i++){
-		for(j = 0; j< 5; j++){
-			move(i , LEFTEDGE + j + 3);
-			if(Block[0][0][i][j] == 1)
-				addstr(fill);
-			
+		else{
+			if(crush_check(block_xpos, block_ypos+1,block_rotate) == true){
+						before_inactive_check = 0;
+						key_down();
+			}
+			if(crush_check(block_xpos,block_ypos+1, block_rotate)==false&&(before_inactive_check == 3)){//check_inactive
+				block_inactive();
+				delete_block();				
+			}
+			else if(crush_check(block_xpos,block_ypos+1, block_rotate)==false){
+				before_inactive_check++;
+			}
+
+			usleep(500000);
 		}
-	}
-	
-	refresh();
-	sleep(200);
-	
 		
+		//refresh(); 
+	}
+}
+void draw_Borad(int y_pos, int x_pos){
+	char wall[] = "@";	
+	char blank[] = "   ";
+	char fill[] = "*";
+	for(int i = 0; i<40;i++){
+		for(int j = 0; j<32; j++){
+			if(Board[i/2][j/2]!=Real_game_Board[i/2][j/2]){
+				move(y_pos + i, x_pos+j);
+				if(Real_game_Board[i/2][j/2] == 0)
+					addstr(blank);
+				else if(Real_game_Board[i/2][j/2] == 1){
+					addstr(wall);
+				}
+				else if(Real_game_Board[i/2][j/2] == 2){
+					addstr(fill);
+				}
+				else if(Real_game_Board[i/2][j/2] == 3){
+					addstr(fill);
+				}
+				refresh();
+			}		
+		}
+	}
+	for(int i = 0 ; i<20; i++)
+		for(int j = 0; j<16; j++)
+			Board[i][j] = Real_game_Board[i][j]; 
+}
+int kbhit(void){
+	int ch = getch();
+	if(ch != ERR){
+		ungetch(ch);
+		return 1;
+	}else{
+		return 0;
+	}
+}
+void new_block(){
+	block_xpos = 5;
+	block_ypos = 0;	
+	block_rotate = 0;
+	for(int i = 0; i<5; i++){			
+		for(int j = 0; j<5; j++){
+				if(Block [0][block_rotate][i][j] == 2)
+					Real_game_Board[block_ypos+i][block_xpos+j] = 2;
+		}		
+	}
+	new_block_flag = 0;
+	
+}
+void key_left(){
+	for(int i = 0; i< 5; i++){
+		for(int j = 0; j< 5; j++){
+			if(Block[0][block_rotate][i][j] == 2){
+				Real_game_Board[block_ypos+i][block_xpos+j] = 0;
+			}
+		}
+	}
+	block_xpos--;
+	for(int i = 0; i< 5; i++){
+		for(int j = 0; j< 5; j++){
+			if(Block[0][block_rotate][i][j] == 2){
+				Real_game_Board[block_ypos+i][block_xpos+j] = 2;
+			}
+		}
+	}
+}
+void key_right(){
+	for(int i = 0; i< 5; i++){
+		for(int j = 0; j< 5; j++){
+			if(Block[0][block_rotate][i][j] == 2){
+				Real_game_Board[block_ypos+i][block_xpos+j] = 0;
+			}
+		}
+	}
+	block_xpos++;
+	for(int i = 0; i< 5; i++){
+		for(int j = 0; j< 5; j++){
+			if(Block[0][block_rotate][i][j] == 2){
+				Real_game_Board[block_ypos+i][block_xpos+j] = 2;
+			}
+		}
+	}
+}
+void key_down(){
+	for(int i = 0; i< 5; i++){
+		for(int j = 0; j< 5; j++){
+			if(Block[0][block_rotate][i][j] == 2){
+				Real_game_Board[block_ypos+i][block_xpos+j] = 0;
+			}
+		}
+	}
+	block_ypos++;
+	for(int i = 0; i< 5; i++){
+		for(int j = 0; j< 5; j++){
+			if(Block[0][block_rotate][i][j] == 2){
+				Real_game_Board[block_ypos+i][block_xpos+j] = 2;
+			}
+		}
+	}
+}
+void key_up(){
+	for(int i = 0; i< 5; i++){
+		for(int j = 0; j< 5; j++){
+			if(Block[0][block_rotate][i][j] == 2){
+				Real_game_Board[block_ypos+i][block_xpos+j] = 0;
+			}
+		}
+	}
+	block_rotate++;
+	block_rotate %= 4;
+	for(int i = 0; i< 5; i++){
+		for(int j = 0; j< 5; j++){
+			if(Block[0][block_rotate][i][j] == 2){
+				Real_game_Board[block_ypos+i][block_xpos+j] = 2;
+			}
+		}
+	}
+}
+int crush_check(int check_bx, int check_by, int check_rotate){
+	for(int i = 0; i <5; i++){
+		for(int j = 0; j<5; j++){
+			if((Block[0][check_rotate][i][j]==2)&&((Real_game_Board[check_by+i][check_bx+j]==3)||(Real_game_Board[check_by+i][check_bx+j]==1)))
+				return false;
+		}
+	}
+	return true;
+}
+void block_inactive(){
+	for(int i = 0; i< 5; i++){
+		for(int j = 0; j< 5; j++){
+			if(Block[0][block_rotate][i][j] == 2){
+				Real_game_Board[block_ypos+i][block_xpos+j] = 3;
+			}
+		}
+	}
+	new_block_flag = 1;
 }
 
-void b()
-{
-	
+void delete_block(){
+	int sum = 0;
+	for(int i = 19; i>=0; i--){
+		sum = 0;
+		for(int j = 0; j<16; j++){
+			sum += Real_game_Board[i][j];
+		}
+		if(sum == 44)
+		{
+			for(int k = i; k>0;k--)
+			{
+				for(int s = 0; s< 16; s++)
+				{
+					Real_game_Board[k][s] = Real_game_Board[k-1][s];
+				}
+			}
+		}
+	}
+}
+
+
+void b(){
 	clear();
 	mvprintw(30,60," 안녕하세요 황보승우입니다.");
 //	mode=1;
 }
 
-void c()
-{
-	
+void c(){
 	endwin();
 //	mode=1;
 }
+
